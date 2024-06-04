a5967afadb1743258ca030a601bbe5c30a16848f
ishmeal <theoleehusky@gmail.com>
2024-06-04 16:47:37 -0400

    polished

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# mv .env.example .env
-# fill in api key
+# Fill in api key:
+# cp .env.example .env
 
-# sudo chown $USER . -R && docker compose build && docker compose up
+# Run with changes:
+# sudo chmod 777 postgres-data browser -R && docker compose build && docker compose up
 
 KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ4NjA3MTIzODksInN1YiI6Im9fMjU3OTk1MjAzOTgwODA4NTk2In0.T6Fq_hcUCFJQwQs2jwz2L8rf1iiKNvJvLqQL1GLK8r8"
 
@@ -33,6 +34,3 @@ curl -X POST -H 'Content-Type: application/json' -H "x-api-key: ${KEY}" -d '{
 #     "navigation_payload": "",
 #     "proxy_location": "NONE"
 # }' http://0.0.0.0:8000/api/v1/tasks
-
-# TODO:
-# - disable video
